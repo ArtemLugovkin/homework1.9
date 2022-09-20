98cb80832a4b1df58f0bd620433901fc248268f9
ArtemLugovkin <artem-lugovkin@mail.ru>
2022-09-20 20:51:38 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int[] weights = new int[12];
+        weights[0] = 90;
+        int januaryWeght = weights[0];
+        System.out.println(januaryWeght);
+        System.out.println(weights[0]);
+        System.out.println(weights[4]);
+        int january = 0;
+        System.out.println(weights[january]);
+        for (int i = 0; i < 12; i++) {
+            System.out.println(weights[i]);
+        }
+
+    }
+}
